ef1ab09735ab46366c0be1cad80d81c2754780d8
Bonjour-1 <okazakishio951@gmail.com>
2026-07-21 09:42:38 +0800

    fix(window): preserve native fullscreen from maximized

--- lib/services/fullscreen_window_service.dart
+++ lib/services/fullscreen_window_service.dart
@@ -2,7 +2,10 @@ import 'package:window_manager/window_manager.dart';
 
 abstract class FullscreenWindowPlatform {
   Future<bool> isFullScreen();
+  Future<bool> isMaximized();
+  Future<void> maximize();
   Future<void> setFullScreen(bool value);
+  Future<void> unmaximize();
 }
 
 class FullscreenWindowService {
@@ -10,18 +13,23 @@ class FullscreenWindowService {
     : _platform = platform ?? _WindowManagerFullscreenPlatform();
 
   final FullscreenWindowPlatform _platform;
+  bool _restoreMaximized = false;
 
   Future<void> toggle() async {
     if (await _platform.isFullScreen()) {
       await exit();
       return;
     }
+    _restoreMaximized = await _platform.isMaximized();
+    if (_restoreMaximized) await _platform.unmaximize();
     await _platform.setFullScreen(true);
   }
 
   Future<void> exit() async {
     if (!await _platform.isFullScreen()) return;
     await _platform.setFullScreen(false);
+    if (_restoreMaximized) await _platform.maximize();
+    _restoreMaximized = false;
   }
 }
 
@@ -30,5 +38,14 @@ class _WindowManagerFullscreenPlatform implements FullscreenWindowPlatform {
   Future<bool> isFullScreen() => windowManager.isFullScreen();
 
   @override
+  Future<bool> isMaximized() => windowManager.isMaximized();
+
+  @override
+  Future<void> maximize() => windowManager.maximize();
+
+  @override
   Future<void> setFullScreen(bool value) => windowManager.setFullScreen(value);
+
+  @override
+  Future<void> unmaximize() => windowManager.unmaximize();
 }
